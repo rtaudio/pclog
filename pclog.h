a01b1619ba7bd43b940adca8dd2e287b203f056c
Fabian Schlieper <fabian@schlieper.email>
2017-11-07 22:01:53 +0100

    LOG_* macros

--- pclog.h
+++ pclog.h
@@ -20,115 +20,129 @@ LOG(logDEBUG) << "h4x0r";
 #ifdef _WIN32
 #include<Windows.h>
 #else
+
 #include <sys/time.h>
+
 #endif
 
 #define LOG(level) if (level > pclog::Log::ReportingLevel()) ; else pclog::Log(level).get()
-	
+#define LOG_E  LOG(logERROR)
+#define LOG_I  LOG(logINFO)
+#define LOG_D  LOG(logDEBUG)
+
 enum PCLogLevel {
-	logERROR, logWARNING, logINFO, logDEBUG,
-	logDEBUG1, logDEBUG2, logDEBUG3, logDEBUG4
+    logERROR, logWARNING, logINFO, logDEBUG,
+    logDEBUG1, logDEBUG2, logDEBUG3, logDEBUG4
 };
 
 namespace pclog {
 
-	inline std::mutex &mutex()
-	{
-		static std::mutex mutex;
-		return mutex;
-	}
+    inline std::mutex &mutex() {
+        static std::mutex mutex;
+        return mutex;
+    }
 
 
-	inline std::string getErrorString(int rc) {
-		char errmsg[1024];
+    inline std::string getErrorString(int rc) {
+        char errmsg[1024];
 #ifdef _WIN32
-		::strerror_s(errmsg, rc);
+        ::strerror_s(errmsg, rc);
 #else
-		strerror_r(rc, errmsg, sizeof errmsg);
+        strerror_r(rc, errmsg, sizeof errmsg);
 #endif
-		return errmsg;
-	}
-
+        return errmsg;
+    }
 
 
 #ifdef ANDROID
+
 #include <android/log.h>
+
 #endif
 
-class Log
-{
-public:
-	const PCLogLevel lv;
+    class Log {
+    public:
+        const PCLogLevel lv;
 #ifdef _WIN32
-	CONSOLE_SCREEN_BUFFER_INFO scbi;
+        CONSOLE_SCREEN_BUFFER_INFO scbi;
 #endif
 
 #ifdef ANDROID
-	std::ostringstream os;
+        std::ostringstream os;
 #endif
 
-	inline Log(PCLogLevel level = logINFO) : lv(level) {
-		mutex().lock(); // TODO: this is very slow, should use buffer
-	}
-	
-	~Log() {
+        inline Log(PCLogLevel level = logINFO) : lv(level) {
+            mutex().lock(); // TODO: this is very slow, should use buffer
+        }
+
+        ~Log() {
 #if !defined(_WIN32) && !defined(ANDROID)
-		if (lv == logERROR || lv == logWARNING)
-			std::cout << "\e[0m ";
+            if (lv == logERROR || lv == logWARNING)
+                std::cout << "\e[0m ";
 #endif
 
 #ifdef ANDROID
-		int ap = ANDROID_LOG_INFO;
-		switch (lv) {
-		case logERROR: ap = ANDROID_LOG_ERROR; break;
-		case logWARNING: ap = ANDROID_LOG_WARN; break;
-		}
-		__android_log_print(ap,
-			"ulltra",
-			"%s",
-			os.str().c_str());
+            int ap = ANDROID_LOG_INFO;
+            switch (lv) {
+                case logERROR:
+                    ap = ANDROID_LOG_ERROR;
+                    break;
+                case logWARNING:
+                    ap = ANDROID_LOG_WARN;
+                    break;
+                case logDEBUG:
+                    ap = ANDROID_LOG_DEBUG;
+                    break;
+                default:
+                    ap = ANDROID_LOG_INFO;
+                    break;
+            }
+            __android_log_print(ap,
+                                "ulltra",
+                                "%s",
+                                os.str().c_str());
 #else
-		std::cout << std::endl << std::flush;
+            std::cout << std::endl << std::flush;
 #endif
-		
+
 #ifdef _WIN32
-		if (lv == logERROR || lv == logWARNING)
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), scbi.wAttributes);
+            if (lv == logERROR || lv == logWARNING)
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), scbi.wAttributes);
 #endif
-		mutex().unlock();
-	}
-	inline std::ostream& get() {
+            mutex().unlock();
+        }
+
+        inline std::ostream &get() {
 
 #ifdef ANDROID
-		std::ostream& ls(this->os);
+            std::ostream &ls(this->os);
 #else
-		std::ostream& ls(std::cout);
+            std::ostream& ls(std::cout);
 #endif
 
-		std::string cl;
+            std::string cl;
 
 #ifdef _WIN32
-		if (lv == logERROR || lv == logWARNING) {
-			GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &scbi);
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ((lv == logWARNING) ? (FOREGROUND_GREEN|FOREGROUND_RED) : FOREGROUND_RED) | FOREGROUND_INTENSITY);
-		}
+            if (lv == logERROR || lv == logWARNING) {
+                GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &scbi);
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ((lv == logWARNING) ? (FOREGROUND_GREEN|FOREGROUND_RED) : FOREGROUND_RED) | FOREGROUND_INTENSITY);
+            }
 #elif !defined(ANDROID)
-		if (lv == logERROR)
-			cl = "\e[91m";
-		else if (lv == logWARNING)
-			cl = "\e[93m";
+            if (lv == logERROR)
+                cl = "\e[91m";
+            else if (lv == logWARNING)
+                cl = "\e[93m";
 #endif
-		ls << (cl + std::string(lv > logDEBUG ? (lv - logDEBUG)*2 : 0, ' '));
-		return ls;
-	}
+            ls << (cl + std::string(lv > logDEBUG ? (lv - logDEBUG) * 2 : 0, ' '));
+            return ls;
+        }
 
-	inline static PCLogLevel ReportingLevel() {
-		return logDEBUG3; // logDebugHttp;
-	}
-};
+        inline static PCLogLevel ReportingLevel() {
+            return logDEBUG3; // logDebugHttp;
+        }
+    };
 
 
-	
 }
 
 #endif
