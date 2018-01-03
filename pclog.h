feabc068a245a0905dc663dc476d99ccee3f3d33
Fabian Schlieper <fabian@schlieper.email>
2018-01-03 13:41:30 +0100

    windows.h

--- pclog.h
+++ pclog.h
@@ -1,5 +1,4 @@
-#ifndef _PCLOG_HEADER_
-#define _PCLOG_HEADER_
+#pragma once
 
 /*
 An efficient logging class that works on Linux, Windows and Android
@@ -18,131 +17,151 @@ LOG(logDEBUG) << "h4x0r";
 #include <mutex>
 
 #ifdef _WIN32
-#include<Windows.h>
+#include<windows.h>
 #else
-
 #include <sys/time.h>
-
 #endif
+
+#if defined(ANDROID) && !defined(PCLOG_ANDROID_USE_STDOUT)
+#include <android/log.h>
+#define PCLOG_ANDROID
+#endif
+
+#define PCLOG_USE_BUFFER
 
 #define LOG(level) if (level > pclog::Log::ReportingLevel()) ; else pclog::Log(level).get()
 #define LOG_E  LOG(logERROR)
 #define LOG_I  LOG(logINFO)
+#define LOG_W  LOG(logWARNING)
 #define LOG_D  LOG(logDEBUG)
+#define LOG_V  LOG(logVERBOSE)
+
+#include "to_string.h"
 
 enum PCLogLevel {
-    logERROR, logWARNING, logINFO, logDEBUG,
-    logDEBUG1, logDEBUG2, logDEBUG3, logDEBUG4
+	logERROR, logWARNING, logINFO, logDEBUG,
+	logDEBUG1, logDEBUG2, logDEBUG3, logDEBUG4, logVERBOSE
 };
 
 namespace pclog {
 
-    inline std::mutex &mutex() {
-        static std::mutex mutex;
-        return mutex;
-    }
+	inline std::mutex &mutex() {
+		static std::mutex mutex;
+		return mutex;
+	}
 
-
-    inline std::string getErrorString(int rc) {
-        char errmsg[1024];
 #ifdef _WIN32
-        ::strerror_s(errmsg, rc);
+
+	inline std::string getErrorString2(int err = 0) {
+//		err = err ? err : WSAGetLastError();
+
+		LPVOID lpMsgBuf;
+		DWORD dw = GetLastError();
+
+		FormatMessage(
+				FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+				NULL, dw, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR) &lpMsgBuf, 0, NULL
+		);
+
+
+		std::string msg((LPTSTR) lpMsgBuf);
+		LocalFree(lpMsgBuf);
+
+		msg += " (" + std::to_string(dw) + ")";
+
+		return msg;
+	}
+
+#endif
+
+	inline std::string getErrorString(int rc) {
+		char errmsg[1024];
+#ifdef _WIN32
+		::strerror_s(errmsg, sizeof errmsg, rc);
 #else
-        strerror_r(rc, errmsg, sizeof errmsg);
+		strerror_r(rc, errmsg, sizeof errmsg);
 #endif
-        return errmsg;
-    }
+		return errmsg;
+	}
 
 
-#ifdef ANDROID
-
-#include <android/log.h>
-
+	class Log {
+	public:
+		const PCLogLevel lv;
+#ifdef PCLOG_USE_BUFFER
+		std::ostringstream buf;
 #endif
 
-    class Log {
-    public:
-        const PCLogLevel lv;
 #ifdef _WIN32
-        CONSOLE_SCREEN_BUFFER_INFO scbi;
+		CONSOLE_SCREEN_BUFFER_INFO scbi;
 #endif
+
+		inline Log(PCLogLevel level = logINFO) : lv(level) {
+
+		}
 
 #ifdef ANDROID
-        std::ostringstream os;
+		inline int androidLogLevel() {
+			switch (lv) {
+				case logERROR:
+					return ANDROID_LOG_ERROR;
+				case logWARNING:
+					return ANDROID_LOG_WARN;
+				case logINFO:
+					return ANDROID_LOG_INFO;
+				case logDEBUG:
+					return ANDROID_LOG_DEBUG;
+				case logVERBOSE:
+					return ANDROID_LOG_VERBOSE;
+				default:
+					return ANDROID_LOG_DEFAULT;
+			}
+		}
 #endif
 
-        inline Log(PCLogLevel level = logINFO) : lv(level) {
-            mutex().lock(); // TODO: this is very slow, should use buffer
-        }
+		~Log() {
+			mutex().lock();
+			std::ostream &os((lv == logERROR || lv == logWARNING) ? std::cerr : std::cout);
 
-        ~Log() {
-#if !defined(_WIN32) && !defined(ANDROID)
-            if (lv == logERROR || lv == logWARNING)
-                std::cout << "\e[0m ";
+#ifdef _WIN32
+			if (lv == logERROR || lv == logWARNING) {
+				GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &scbi);
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
+										((lv == logWARNING) ? (FOREGROUND_GREEN | FOREGROUND_RED) : FOREGROUND_RED) |
+										FOREGROUND_INTENSITY);
+			}
+#elif !defined(PCLOG_ANDROID)
+			if (lv == logERROR)
+				os << "\e[91m";
+			else if (lv == logWARNING)
+				os <<  "\e[93m";
 #endif
 
-#ifdef ANDROID
-            int ap = ANDROID_LOG_INFO;
-            switch (lv) {
-                case logERROR:
-                    ap = ANDROID_LOG_ERROR;
-                    break;
-                case logWARNING:
-                    ap = ANDROID_LOG_WARN;
-                    break;
-                case logDEBUG:
-                    ap = ANDROID_LOG_DEBUG;
-                    break;
-                default:
-                    ap = ANDROID_LOG_INFO;
-                    break;
-            }
-            __android_log_print(ap,
-                                "ulltra",
-                                "%s",
-                                os.str().c_str());
+#ifdef PCLOG_ANDROID
+			__android_log_print(androidLogLevel(), "pclog",  "%s", buf.str().c_str());
 #else
-            std::cout << std::endl << std::flush;
-#endif
-
+			os << buf.str();
+			if (lv == logERROR || lv == logWARNING) {
 #ifdef _WIN32
-            if (lv == logERROR || lv == logWARNING)
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), scbi.wAttributes);
-#endif
-            mutex().unlock();
-        }
-
-        inline std::ostream &get() {
-
-#ifdef ANDROID
-            std::ostream &ls(this->os);
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), scbi.wAttributes);
 #else
-            std::ostream& ls(std::cout);
+				os << "\e[0m ";
 #endif
-
-            std::string cl;
-
-#ifdef _WIN32
-            if (lv == logERROR || lv == logWARNING) {
-                GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &scbi);
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ((lv == logWARNING) ? (FOREGROUND_GREEN|FOREGROUND_RED) : FOREGROUND_RED) | FOREGROUND_INTENSITY);
-            }
-#elif !defined(ANDROID)
-            if (lv == logERROR)
-                cl = "\e[91m";
-            else if (lv == logWARNING)
-                cl = "\e[93m";
+			}
+			os << std::endl << std::flush;
+			buf.clear();
 #endif
-            ls << (cl + std::string(lv > logDEBUG ? (lv - logDEBUG) * 2 : 0, ' '));
-            return ls;
-        }
+			mutex().unlock();
+		}
 
-        inline static PCLogLevel ReportingLevel() {
-            return logDEBUG3; // logDebugHttp;
-        }
-    };
+		inline std::ostream &get() {
+			// add space prefix based on debug level
+			buf << std::string(lv > logDEBUG ? (lv - logDEBUG) * 2 : 0, ' ');
+			return buf;
+		}
 
-
+		inline static PCLogLevel ReportingLevel() {
+			return logDEBUG3; // logDebugHttp;
+		}
+	};
 }
-
-#endif
